37c30b87407f2b947cb61f438e1795294b92dff6
leodevillibra <leodevillibra@gmail.com>
2013-04-19 22:10:14 +0530

    Update emoticons.js

--- prettyfacebookchat/emoticons.js
+++ prettyfacebookchat/emoticons.js
@@ -3,7 +3,7 @@
     Chrome Extension for improve facebook chat
     Copyright (C) 2012  Luca Rainone <luca.rainone@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
+     program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
